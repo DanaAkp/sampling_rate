6f5ec2baad94fc95a774ddeee13b1d9508bdd63c
Danagul Akpaeva <aidanagul10@gmail.com>
2019-03-12 17:49:44 +0300

    Добавьте файлы проекта.

---
+++ TVMS/DiscriptiveStatistics.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVMS
+{
+
+    class DiscriptiveStatistics
+    {
+
+        /// <summary>
+        /// Нахождение среднего для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Average(double[] arr)
+        {
+            return arr.Sum() / arr.Length;
+        }
+        /// <summary>
+        /// Нахождение стандартной ошибки для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double StandartError(double[] arr)
+        {
+            return StandartDeviation(arr) / Math.Sqrt(arr.Length);
+        }
+        /// <summary>
+        /// Нахождение моды для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Fashion(double[] arr)
+        {
+            var dic = arr.GroupBy(x => x).Where(g => g.Count() > 1).Select(y => new { Element = y.Key, Count = y.Count() }).ToList();
+            var w = from v in dic orderby v.Count select v;
+            return w.Last().Element;
+        }
+        /// <summary>
+        /// Нахождение максимального элемента для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Max(double[] arr)
+        {
+            return arr.Max();
+        }
+        /// <summary>
+        /// Нахождение минимального элемента для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Min(double[] arr)
+        {
+            return arr.Min();
+        }
+        /// <summary>
+        /// Нахождение эксцеса для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Excess(double[] arr)
+        {
+            double aver = Average(arr);
+            double res = 0;
+            foreach (double x in arr)
+                res += (x - aver) * (x - aver) * (x - aver) * (x - aver);
+            double n = arr.Length;
+            res /= Math.Pow(StandartDeviation(arr), 4);
+            res *= (n * (n + 1)) / ((n - 1) * (n - 2) * (n - 3));
+            res -= 3 * (n - 1) * (n - 1) / ((n - 2) * (n - 3));
+            return res;
+        }
+        /// <summary>
+        /// Нахождение суммы всех элементов для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Amount(double[] arr)
+        {
+            return arr.Sum();
+        }
+        /// <summary>
+        /// Нахождение интервала для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Interval(double[] arr)
+        {
+            return Max(arr) - Min(arr);
+        }
+        /// <summary>
+        /// Нахождение стандартного отклонения для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double StandartDeviation(double[] arr)
+        {
+            return Math.Sqrt(Dispersion(arr));
+        }
+        /// <summary>
+        /// Нахождение асимметрии для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Asymmetry(double[] arr)
+        {
+            double aver = Average(arr);
+            double res = 0;
+            foreach (double x in arr)
+                res += (x - aver) * (x - aver) * (x - aver);
+            res /= Math.Pow(StandartDeviation(arr), 3);
+            res *= (double)arr.Length / (double)((arr.Length - 1) * (arr.Length - 2));
+            return res;
+        }
+        /// <summary>
+        /// Нахождение медианы для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Median(double[] arr)
+        {
+            var s = from t in arr orderby t select t;
+            var d = s.Select((elem, index) => new { index, elem = arr[index] });
+
+            var e = s.ToArray().GetValue(s.ToArray().Count() / 2);
+
+            if (arr.Length % 2 != 0) return (double)e;
+
+            var f1 = s.ToArray().GetValue(s.ToArray().Count() / 2 - 1);
+            return ((double)f1 + (double)e) / 2;
+            //return f1;
+        }
+        /// <summary>
+        /// Нахождение дисперсии для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        /// <returns></returns>
+        public static double Dispersion(double[] arr)
+        {
+            double aver = Average(arr);
+            double res = 0;
+            foreach (double x in arr)
+                res += (x - aver) * (x - aver);
+            res /= arr.Length - 1;
+            return res;
+        }
+        /// <summary>
+        /// Вывод описательной статистики для какой-то совокупности
+        /// </summary>
+        /// <param name="arr">Какая-то совокупность</param>
+        public static string Output_descriptive_statistics(double[] arr)
+        {
+            string s = "Описательная статистика: ";
+            s += "\nСреднее: " + Average(arr);
+            s += "\nСумма: " + Amount(arr);
+            s += "\nСтандартная ошибка: " + StandartError(arr);
+            s += "\nМедиана: " + Median(arr);
+            s += "\nМода: " + Fashion(arr);
+            s += "\nСтандартное отклонение: " + StandartDeviation(arr);
+            s += "\nДисперсия выборки: " + Dispersion(arr);
+            s += "\nЭксцесс: " + Excess(arr);
+            s += "\nАсимметричность: " + Asymmetry(arr);
+            s += "\nИнтервал: " + Interval(arr);
+            s += "\nМинимум: " + Min(arr);
+            s += "\nМаксимум: " + Max(arr);
+            s += "\nСчет: " + arr.Length;
+            return s;
+        }
+
+        public static double[] Rationing_MaxMin(double[] arr)
+        {
+            double MM = Max(arr) - Min(arr);
+            var s = arr.Select(x => x / MM).ToArray();
+            return (double[])s;
+        }
+        /// <summary>
+        /// Нормирование с помощью дисперсии
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void Rationing_Dispersive(double[][] arr)
+        {
+
+        }
+        public static string Output(double[] arr)
+        {
+            string s = "";
+            for (int i = 0; i < arr.Length; i++)
+            {
+                s+=string.Format("{0:F2} ", arr[i]);
+            }
+            s += "\n";
+            return s;
+        }
+        public static double[] GetSample(string source)
+        {
+            string[] s = source.Trim().Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Replace('.', ',')).ToArray();
+            double[] arr = new double[s.Length];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+
+                
+arr[i] = double.Parse(s[i]);
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ TVMS/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TVMS
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string s = "";
+        double[] sampleSource;
+        double[] sampleRationing;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog FileOT = new OpenFileDialog();
+            FileOT.Filter = "All files (*.*)|*.*|TXT text (*.txt)|*.txt";
+            if (FileOT.ShowDialog() == true)
+            {
+                Stream ms = new FileStream(FileOT.FileName, FileMode.Open);
+                byte[] array = new byte[ms.Length];
+                ms.Read(array, 0, array.Length);
+                string buf = Encoding.Default.GetString(array);
+                s = buf.ToLower();
+            }
+            sampleSource = DiscriptiveStatistics.GetSample(s);
+            tbDiscrStat_source.Text = DiscriptiveStatistics.Output_descriptive_statistics(sampleSource);
+            tblSourceSample.Text = DiscriptiveStatistics.Output(sampleSource);
+        }
+
+        private void BtnRationing_Click(object sender, RoutedEventArgs e)
+        {
+            sampleRationing = DiscriptiveStatistics.Rationing_MaxMin(sampleSource);
+            tblRationning.Text = DiscriptiveStatistics.Output(sampleRationing);
+            tbDiscrStat_Ration.Text = DiscriptiveStatistics.Output_descriptive_statistics(sampleRationing);
+        }
+
+        private void BtnSaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sd = new SaveFileDialog();
+            sd.Filter = "All files (*.*)|*.*|TXT text (*.txt)|*.txt";
+            if (sd.ShowDialog() == true)
+            {
+                File.WriteAllText(sd.FileName, DiscriptiveStatistics.Output(sampleRationing));
+            }
+        }
+    }
+}
